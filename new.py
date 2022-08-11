b1331b8e77a888c3589b371f670e8912eece5e95
Aakriti Yadav <aakritiyadav82@gmail.com>
2022-08-11 11:50:40 +0545

    string in python

---
+++ new.py
@@ -0,0 +1,12 @@
+#--> it represent a one line comment nin python 
+
+'''
+it represent a multi-line commenty
+we can write any thing here
+'''
+
+a= 5 #here, a is a variable which stores/holds integer type value i.e 5
+naam = "aakriti"    #here, naam is variable which store/holds string type value i.e "aakriti"
+
+# add python srtring are always inside or double quotes:
+# for e4xample: "aakriti" or 'aakriti'
